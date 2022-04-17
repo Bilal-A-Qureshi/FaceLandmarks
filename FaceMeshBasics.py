4b947b8de4158809142873a7d256ad2a73c07864
Bilal Ahmed Qureshi <77246955+Bilal-A-Qureshi@users.noreply.github.com>
2022-04-17 17:18:22 +0200

    Add files via upload

---
+++ FaceMeshBasics.py
@@ -0,0 +1,32 @@
+import cv2
+import mediapipe as mp
+import time
+
+cap = cv2.VideoCapture("Videos/1.mp4")
+pTime=0
+
+mpDraw = mp.solutions.drawing_utils
+mpFaceMesh = mp.solutions.face_mesh
+faceMesh = mpFaceMesh.FaceMesh(max_num_faces=1)
+drawSpec = mpDraw.DrawingSpec(thickness=2, circle_radius=2)
+
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = faceMesh.process(imgRGB)
+
+    if results.multi_face_landmarks:
+        for faceLms in results.multi_face_landmarks:
+            mpDraw.draw_landmarks(img, faceLms, mpFaceMesh.FACEMESH_TESSELATION,drawSpec)
+
+    img = cv2.resize(img, (400, 650), fx=0, fy=0, interpolation=cv2.INTER_CUBIC)
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    cv2.putText(img, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN,
+                3, (255, 0, 0), 3)
+
+    cv2.imshow("img",img)
+    cv2.waitKey(1)
